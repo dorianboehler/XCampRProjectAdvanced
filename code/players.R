70964247c5e50a70687866047d59bf76b5422c6a
dorianboehler <71817806+dorianboehler@users.noreply.github.com>
2023-05-07 16:01:52 +0200

    Combine and improve the code

---
+++ code/players.R
@@ -0,0 +1,303 @@
+##################################################
+# Script to scrape and clean the list of players from atptour.com
+##################################################
+
+# SET UP ------------------------------------------------------------------
+# Clean the environment
+remove(list = ls())
+
+# Load the necessary packages
+library(insight)
+library(rvest)
+library(tidyverse)
+library(xml2)
+
+# SCRAPE THE LIST OF PLAYERS FROM THE WEBSITE -----------------------------
+# In order to have a complete list of players that have played since 1973, 
+# we will download the year-end ranking for every year. Unfortunately,
+# we need to provide dates from the drop-down menu in the links to the year-end rankings.
+# For this reason, we will first scrape these dates.
+
+# Download the website
+website <- read_html('https://www.atptour.com/en/rankings/singles')
+
+# Loop through the drop-down menu and save the dates in a data frame
+numberOfDates <- length(xml_find_all(website, xpath = '//*[@id="filterHolder"]/div/div/div[3]/div/ul/li'))
+
+dates <- data.frame(matrix(nrow = numberOfDates - 1, ncol = 2))
+colnames(dates) <- c('date', 'year')
+
+for(i in 1:(numberOfDates - 1)) {
+  dates$date[i] <- html_elements(website, xpath = paste0('//*[@id="filterHolder"]/div/div/div[3]/div/ul/li[', i + 1, ']')) %>%
+    html_attr('data-value')
+}
+  
+remove(website, i, numberOfDates)
+
+# Extract the year
+dates$year <- substr(dates$date, 1, 4)
+
+# Keep only the latest date per year
+dates <- dates %>%
+  group_by(year) %>%
+  slice(1)
+
+# Remove the year
+dates <- dates %>%
+  ungroup() %>%
+  select(-year)
+
+# We are now ready to download the year-end rankings.
+
+# Create the links
+dates$link <- paste0('https://www.atptour.com/en/rankings/singles?rankRange=1-5000&rankDate=', dates$date)
+
+# Loop through the links and save the names and links of the players (takes about 35 minutes)
+players <- data.frame(matrix(nrow = 0, ncol = 2))
+colnames(players) <- c('name', 'link')
+
+for(link in dates$link) {
+  # Download the website
+  website <- read_html(link)
+  
+  # Count the number of players
+  numberOfPlayers <- length(xml_find_all(website, xpath = '//*[@id="player-rank-detail-ajax"]/tbody/tr'))
+  
+  # Save the names and links of the players in a temporary data frame
+  temp <- data.frame(matrix(nrow = numberOfPlayers, ncol = 2))
+  colnames(temp) <- c('name', 'link')
+  
+  for(i in 1:numberOfPlayers) {
+    temp$name[i] <- html_elements(website, xpath = paste0('//*[@id="player-rank-detail-ajax"]/tbody/tr[', i, ']/td[4]/span/a')) %>%
+      html_attr('ga-label')
+    
+    temp$link[i] <- html_elements(website, xpath = paste0('//*[@id="player-rank-detail-ajax"]/tbody/tr[', i, ']/td[4]/span/a')) %>%
+      html_attr('href')
+  }
+  
+  # Add the temporary data frame to the final data frame
+  players <- rbind(players, temp)
+  
+  # Display the progress
+  print(paste('Year', substr(dates$date[which(dates$link == link)], 1, 4), 'done'))
+}
+
+remove(dates, temp, website, i, link, numberOfPlayers)
+
+# Remove duplicates (most players played for more than one year)
+players <- distinct(players)
+
+# Remove the unknown players
+players <- filter(players, name != 'Unknown Unknown')
+
+# Remove the last part of the links so that we can use them generally
+players$link <- substr(players$link, 1, nchar(players$link) - 8)
+
+# Remove the quotation marks from the links (as they do not work otherwise)
+players$link <- gsub('"', '', players$link, fixed = TRUE)
+
+# Remove certain dots from the links (as they do not work otherwise)
+players$link <- gsub('./', '/', players$link, fixed = TRUE)
+
+# Since there are some players with the same name, 
+# we need additional information on the players in order to uniquely identify them later.
+# Besides, this information will be interesting in the analysis later.
+
+# Create an empty csv file to which we will append the rows one by one (this is more efficient)
+file.create('data/players.csv')
+file <- file('data/players.csv')
+
+writeLines('name\tlink\tactive\tcountry\tbirthday\tturnedPro\tweight\theight\tlefty\toneHandedBackhand', file)
+
+close(file)
+
+remove(file)
+
+# Create an empty data frame for the errors
+errors <- data.frame(matrix(nrow = 0, ncol = 3))
+colnames(errors) <- c('name', 'link', 'error')
+
+# Loop through the players and download the information
+for(i in 1:nrow(players)) {
+  tryCatch(expr = {
+    # Download the website
+    website <- read_html(paste0('https://www.atptour.com', players$link[i], 'overview'))
+    
+    # Extract the information
+    active <- !is.na(str_extract(html_elements(website, xpath = '//*[@id="playerProfileHero"]/div[2]/div[1]/div/div[3]/div[1]/div[2]') %>%
+                                   html_text(),
+                                 '[:digit:]+'))
+    
+    country <- html_elements(website, xpath = '//*[@id="playerProfileHero"]/div[2]/div[1]/div/div[3]/div[2]/div[2]') %>%
+      html_text()
+    if(length(country) == 0) {
+      country <- NA
+    }
+    
+    temp <- html_elements(website, xpath = '//*[@id="playerProfileHero"]/div[2]/div[2]') %>%
+      html_table()
+    temp <- as.data.frame(temp)
+    
+    birthday <- str_extract(temp[1, 1], '[:digit:]{4}.[:digit:]{2}.[:digit:]{2}')
+    
+    turnedPro <- str_extract(temp[1, 2], '[:digit:]{4}')
+    
+    weight <- str_extract(temp[1, 3], '[:digit:]{2,3}?kg')
+    
+    height <- str_extract(temp[1, 4], '[:digit:]{3}cm')
+    
+    lefty <- str_extract(temp[2, 2], '.*(?=,)')
+    
+    oneHandedBackhand <- str_extract(temp[2, 2], '(?<=, ).*')
+    
+    # Append the row to the csv file
+    temp <- data.frame(name = players$name[i],
+                       link = players$link[i],
+                       active = active,
+                       country = country,
+                       birthday = birthday,
+                       turnedPro = turnedPro,
+                       weight = weight,
+                       height = height,
+                       lefty = lefty,
+                       oneHandedBackhand = oneHandedBackhand)
+    
+    write.table(temp, file = 'data/players.csv', append = TRUE, sep = '\t', row.names = FALSE, col.names = FALSE, fileEncoding = 'UTF-8')
+    
+    # Display the progress
+    print(paste('Player', i, 'out of', nrow(players), 'done.'))
+  }, error = function(e) {
+    temp <- data.frame(name = players$name[i],
+                       link = players$link[i],
+                       error = as.character(e))
+    
+    errors <<- rbind(errors, temp) # Be careful with super assignments!
+    
+    # Display the error in red
+    print_colour(paste('Error:', as.character(e)), 'red')
+  })
+} # We can ignore the warning messages. They are in the errors.
+
+remove(temp, website, active, birthday, country, 
+       height, i, lefty, oneHandedBackhand, turnedPro,
+       weight)
+
+# Try the links that did not work again
+tryAgain <- errors
+errors <- data.frame(matrix(nrow = 0, ncol = 3))
+colnames(errors) <- c('name', 'link', 'error')
+
+for(i in 1:nrow(tryAgain)) {
+  tryCatch(expr = {
+    # Download the website
+    website <- read_html(paste0('https://www.atptour.com', tryAgain$link[i], 'overview'))
+    
+    # Extract the information
+    active <- !is.na(str_extract(html_elements(website, xpath = '//*[@id="playerProfileHero"]/div[2]/div[1]/div/div[3]/div[1]/div[2]') %>%
+                                   html_text(),
+                                 '[:digit:]+'))
+    
+    country <- html_elements(website, xpath = '//*[@id="playerProfileHero"]/div[2]/div[1]/div/div[3]/div[2]/div[2]') %>%
+      html_text()
+    if(length(country) == 0) {
+      country <- NA
+    }
+    
+    temp <- html_elements(website, xpath = '//*[@id="playerProfileHero"]/div[2]/div[2]') %>%
+      html_table()
+    temp <- as.data.frame(temp)
+    
+    birthday <- str_extract(temp[1, 1], '[:digit:]{4}.[:digit:]{2}.[:digit:]{2}')
+    
+    turnedPro <- str_extract(temp[1, 2], '[:digit:]{4}')
+    
+    weight <- str_extract(temp[1, 3], '[:digit:]{2,3}?kg')
+    
+    height <- str_extract(temp[1, 4], '[:digit:]{3}cm')
+    
+    lefty <- str_extract(temp[2, 2], '.*(?=,)')
+    
+    oneHandedBackhand <- str_extract(temp[2, 2], '(?<=, ).*')
+    
+    # Append the row to the csv file
+    temp <- data.frame(name = tryAgain$name[i],
+                       link = tryAgain$link[i],
+                       active = active,
+                       country = country,
+                       birthday = birthday,
+                       turnedPro = turnedPro,
+                       weight = weight,
+                       height = height,
+                       lefty = lefty,
+                       oneHandedBackhand = oneHandedBackhand)
+    
+    write.table(temp, file = 'data/players.csv', append = TRUE, sep = '\t', row.names = FALSE, col.names = FALSE, fileEncoding = 'UTF-8')
+  }, error = function(e) {
+    temp <- data.frame(name = tryAgain$name[i],
+                       link = tryAgain$link[i],
+                       error = as.character(e))
+    
+    errors <<- rbind(errors, temp) # Again, be careful with super assignments!
+  })
+} # Again, we can ignore the warning messages. They are in the errors.
+
+remove(temp, tryAgain, website, active, birthday,
+       country, height, i, lefty, oneHandedBackhand,
+       turnedPro, weight)
+
+# Import the resulting list of players
+players <- read.delim('data/players.csv')
+
+# Save the errors and manually investigate them
+write.csv(errors, 'data/errors.csv')
+
+remove(errors)
+
+# Only one error occurred. This error is an HTTP error 404
+# because the player does not have a profile.
+# Let us ignore this player.
+
+# CLEAN THE LIST OF PLAYERS -----------------------------------------------
+# active: Change the data type to numeric
+players$active <- as.numeric(players$active)
+
+# country: Replace empty strings with NA
+players$country[players$country == '' & !is.na(players$country)] <- NA
+
+# country: Change the data type to factor
+players$country <- as.factor(players$country)
+
+# birthday: Change the data type to date
+players$birthday <- as.Date(players$birthday, format = '%Y.%m.%d')
+
+# turnedPro: Change the data type to numeric
+players$turnedPro <- as.numeric(players$turnedPro)
+
+# weight: Remove kg and change the data type to numeric
+players$weight <- as.numeric(substr(players$weight, 1, nchar(players$weight) - 2))
+
+# height: Remove cm and change the data type to numeric
+players$height <- as.numeric(substr(players$height, 1, nchar(players$height) - 2))
+
+# lefty: Create a dummy variable
+players$lefty[players$lefty == 'Ambidextrous' & !is.na(players$lefty)] <- NA
+
+players$lefty[players$lefty == 'Right-Handed' & !is.na(players$lefty)] <- 0
+
+players$lefty[players$lefty == 'Left-Handed' & !is.na(players$lefty)] <- 1
+
+players$lefty <- as.numeric(players$lefty)
+
+# oneHandedBackhand: Create a dummy variable
+players$oneHandedBackhand[players$oneHandedBackhand == 'Unknown Backhand' & !is.na(players$oneHandedBackhand)] <- NA
+
+players$oneHandedBackhand[players$oneHandedBackhand == 'Two-Handed Backhand' & !is.na(players$oneHandedBackhand)] <- 0
+
+players$oneHandedBackhand[players$oneHandedBackhand == 'One-Handed Backhand' & !is.na(players$oneHandedBackhand)] <- 1  
+
+players$oneHandedBackhand <- as.numeric(players$oneHandedBackhand)  
+
+# Save the cleaned list of players
+save(players, file = 'data/playersCleaned.RData')
+
+remove(players)
---
+++ code/report.R
@@ -0,0 +1,381 @@
+##################################################
+# Script to produce a report for a player of choice
+##################################################
+
+# SET UP ------------------------------------------------------------------
+# Clean the environment
+remove(list = ls())
+
+# Load the necessary packages
+library(countrycode)
+library(rvest)
+library(stringr)
+library(tidyverse)
+
+# Set fixed variables
+australienDollarToUSDollar <- 0.6761 # 06.05.2023
+euroToUSDollar <- 1.1210 # 06.05.2023
+poundToUSDollar <- 1.2639 # 06.05.2023
+
+# Import the cleaned list of players
+load('data/playersCleaned.RData')
+
+# CHOOSE A PLAYER FROM THE LIST -------------------------------------------
+# Enter the player's name and birthday
+playerName <- 'Roger Federer'
+playerBirthday <- as.Date('1981-08-08')
+
+# Extract the general information on the player
+overview <- filter(players, name == playerName & birthday == playerBirthday)
+
+# Check whether the player actually exists in the list
+if(nrow(overview) != 1) {
+  stop('The player does not exist in the list. Please make sure that you entered the name and birthday correctly.')
+}
+
+# SCRAPE THE RESULTS OF THE PLAYER ----------------------------------------
+# Download the website
+website <- read_html(paste0('https://www.atptour.com', overview$link, 'player-activity?year=all'))
+
+# Extract all relevant information and put them into a new data frame
+results <- data.frame(matrix(nrow = 0, ncol = 16))
+colnames(results) <- c('tournamentType', 'tournament', 'location', 'date', 'outdoor',
+                       'surface', 'priceMoneyTotal', 'round', 'opponentRank', 'opponent',
+                       'opponentLink', 'win', 'score', 'eventPoints', 'rank',
+                       'priceMoney')
+
+numberOfTournaments <- length(html_elements(website, xpath = '//*[@id="currentTabContent"]/div[3]/div'))
+
+for(i in 1:numberOfTournaments) {
+  tournamentType <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[1]/img')) %>%
+    html_attr('alt')
+  if(tournamentType == "") {
+    tournamentType <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[1]/img')) %>%
+      html_attr('src')
+    
+    if(grepl('grandslam', tournamentType, fixed = TRUE)) {
+      tournamentType <- 'Grand Slam'
+    } else if(grepl('itf', tournamentType, fixed = TRUE)) {
+      tournamentType <- 'ITF'
+    }
+  } # Some tournament types do not have an attribute alt so that we need to extract the information differently.
+  
+  if(length(html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/a'))) > 0) {
+    tournament <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/a')) %>%
+      html_text()
+    tournament <- str_squish(gsub('\\r\\n', '', tournament))
+    
+    location <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/span[1]')) %>%
+      html_text()
+    location <- str_squish(gsub('\\r\\n', '', location))
+    
+    date <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/span[2]')) %>%
+      html_text()
+    date <- str_squish(gsub('\\r\\n', '', date))
+  } else if(length(html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/span'))) == 3) {
+    tournament <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/span[1]')) %>%
+      html_text
+    tournament <- str_squish(gsub('\\r\\n', '', tournament))
+    
+    location <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/span[2]')) %>%
+      html_text()
+    location <- str_squish(gsub('\\r\\n', '', location))
+    
+    date <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[2]/span[3]')) %>%
+      html_text()
+    date <- str_squish(gsub('\\r\\n', '', date))
+  } else {
+    stop('Something unexpected happened')
+  } # There are two different xpaths to the tournament, location, and date.
+  
+  temp <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[3]/table/tbody/tr/td[2]/div[2]/div')) %>%
+    html_text()
+  temp <- str_squish(gsub('\\r\\n', '', temp))
+  outdoor <- substr(temp, 1, 1)
+  
+  surface <- substr(temp, 3, nchar(temp))
+  
+  priceMoneyTotal <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[1]/tbody/tr/td[3]/table/tbody/tr/td[3]/div[2]/div/span')) %>%
+    html_text()
+  priceMoneyTotal <- str_squish(gsub('\\r\\n', '', priceMoneyTotal))
+  
+  temp <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[2]/tbody')) %>%
+    html_table()
+  temp <- as.data.frame(temp)
+  
+  if(nrow(temp) > 0) {
+    round <- temp$X1
+    
+    opponentRank <- temp$X2
+    
+    opponent <- temp$X3
+    
+    win <- temp$X4
+    
+    score <- temp$X5
+    
+    opponentLink <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/table[2]/tbody/tr/td[3]/div[2]/a')) %>%
+      html_attr('href')
+    opponentLink <- substr(opponentLink, 1, nchar(opponentLink) - 8)
+    opponentLink <- append(opponentLink, rep(NA, sum(opponent == 'Bye')))
+    
+    temp <- html_elements(website, xpath = paste0('//*[@id="currentTabContent"]/div[3]/div[', i, ']/div')) %>%
+      html_text()
+    
+    eventPoints <- str_extract(temp, '(?<=This Event Points: ).*?(?=,)')
+    
+    rank <- str_extract(temp, '(?<=ATP Ranking: ).*?(?=,)')
+    
+    priceMoney <- str_extract(temp, '(?<=Prize Money: ).*')
+    
+    temp <- data.frame(tournamentType = tournamentType,
+                       tournament = tournament,
+                       location = location,
+                       date = date,
+                       outdoor = outdoor,
+                       surface = surface,
+                       priceMoneyTotal = priceMoneyTotal,
+                       round = round,
+                       opponentRank = opponentRank,
+                       opponent = opponent,
+                       opponentLink = opponentLink,
+                       win = win,
+                       score = score,
+                       eventPoints = eventPoints,
+                       rank = rank,
+                       priceMoney)
+    
+    results <- rbind(results, temp)
+  } # Some tournaments do not display any match.
+  
+  # Display the progress
+  print(paste('Tournament', i, 'out of', numberOfTournaments, 'done.'))
+}
+
+remove(temp, website, date, eventPoints, i, 
+       location, numberOfTournaments, opponent, opponentLink, outdoor, 
+       priceMoney, priceMoneyTotal, rank, opponentRank, round, 
+       score, surface, tournament, tournamentType, win)
+
+# CLEAN THE RESULTS OF THE PLAYER -----------------------------------------
+# tournamentType: Change the data type to factor
+results$tournamentType <- as.factor(results$tournamentType)
+
+# tournament: If the player played explicitly for his country, it was in the Davis Cup
+results$tournament[grepl(overview$country, results$tournament, fixed = TRUE)] <- 'Davis Cup'
+
+# location: Separate the location into the city and country
+results$city <- NA
+results$country <- NA
+
+for(i in 1:nrow(results)) {
+  location <- results$location[i]
+  
+  if(grepl(',', location, fixed = TRUE)) {
+    results$city[i] <- str_extract(location, '.*?(?=,)')
+    results$country[i] <- gsub('.*, ', '', location)
+  } else {
+    if(is.na(countryname(location, warn = FALSE))) {
+      results$city[i] <- location
+    } else {
+      results$country[i] <- location
+    }
+  }
+}
+
+remove(i, location)
+
+# date: Separate the date into the start and end and change the data type to date
+results <- separate(results, date, c('tournamentStart', 'tournamentEnd'), sep = '-')
+
+for(col in c('tournamentStart', 'tournamentEnd')) {
+  results[[col]] <- str_squish(results[[col]])
+  
+  results[[col]] <- as.Date(results[[col]], format = '%Y.%m.%d')
+}
+
+remove(col)
+
+# outdoor: Create a dummy variable
+results$outdoor <- ifelse(results$outdoor == 'O', 1, 0)
+
+# surface: Change the data type to factor
+results$surface <- as.factor(results$surface)
+
+# priceMoneyTotal: Separate the total price money into the currency and value and change the data type to factor and numeric, respectively
+results$priceMoneyTotal <- gsub(',', '', results$priceMoneyTotal, fixed = TRUE)
+
+results$priceMoneyTotalValue <- str_extract(results$priceMoneyTotal, '[:digit:]+')
+results$priceMoneyTotalValue <- as.numeric(results$priceMoneyTotalValue)
+
+results$priceMoneyTotalCurrency <- str_extract(results$priceMoneyTotal, '.+?(?=[:digit:])')
+results$priceMoneyTotalCurrency <- as.factor(results$priceMoneyTotalCurrency)
+
+# priceMoneyTotal: Convert the total price money into US dollar
+results$priceMoneyTotalUSDollar <- NA
+
+for(i in 1:nrow(results)) {
+  currency <- results$priceMoneyTotalCurrency[i]
+  value <- results$priceMoneyTotalValue[i]
+  
+  if(currency == 'A$' & !is.na(currency)) {
+    results$priceMoneyTotalUSDollar[i] <- value * australienDollarToUSDollar
+  } else if(currency == '€' & !is.na(currency)) {
+    results$priceMoneyTotalUSDollar[i] <- value * euroToUSDollar
+  } else if(currency == '£' & !is.na(currency)) {
+    results$priceMoneyTotalUSDollar[i] <- value * poundToUSDollar
+  } else if(currency == '$' & !is.na(currency)) {
+    results$priceMoneyTotalUSDollar[i] <- value
+  } else {
+    if(!is.na(currency)) {
+      stop('There is a currency that we did not account for.')
+    }
+  }
+}
+
+remove(currency, i, value)
+
+# round: Change the data type to factor
+results$round <- as.factor(results$round)
+
+# opponentRank: Change the data type to numeric
+results$opponentRank[results$opponentRank == '-' & !is.na(results$opponentRank)] <- NA
+
+results$opponentRank <- as.numeric(results$opponentRank)
+
+# opponentLink: Change the link of the opponent in the same way as in the list of players (lines 96 to 100 in players.R)
+results$opponentLink <- gsub('"', '', results$opponentLink, fixed = TRUE)
+
+results$opponentLink <- gsub('./', '/', results$opponentLink, fixed = TRUE)
+
+# win: Create a dummy variable
+results$win[results$win == '' & !is.na(results$win)] <- NA
+
+results$win <- ifelse(results$win == 'W', 1, 0)
+
+# score: Replace empty strings with NA
+results$score[results$score == '' & !is.na(results$score)] <- NA
+
+# score: Create a dummy variable for (W/O) (walkover)
+results$walkover <- NA
+
+results$walkover[!is.na(results$score)] <- 0
+
+results$walkover[results$score == '(W/O)' & !is.na(results$score)] <- 1
+
+results$score[results$score == '(W/O)' & !is.na(results$score)] <- NA
+
+# score: Create a dummy variable for (RET) (retirement)
+results$retirement <- NA
+
+results$retirement[!is.na(results$score)] <- 0
+
+results$retirement[grepl('(RET)', results$score, fixed = TRUE)] <- 1
+
+results$score <- gsub('(RET)', '', results$score, fixed = TRUE)
+results$score <- trimws(results$score)
+
+# score: Create a column for every set
+results <- separate(results, score, c('set1', 'set2', 'set3', 'set4', 'set5'), sep = ' ', fill = 'right')
+
+# score: Remove the number of points in a tiebreak from the score
+for(col in c('set1', 'set2', 'set3', 'set4', 'set5')) {
+  results[[col]][grepl('^(76|67|43|34|10|01)[0-9]+', results[[col]])] <- substr(results[[col]][grepl('^(76|67|43|34|10|01)[0-9]+', results[[col]])], 1, 2)
+}
+
+remove(col)
+
+# score: Separate the score into the player's and opponent's number of games and change the data type to numeric
+for(col in c('Set1', 'Set2', 'Set3', 'Set4', 'Set5')) {
+  results[[paste0('gamesWon', col)]] <- NA
+  results[[paste0('gamesLost', col)]] <- NA
+}
+
+for(i in 1:nrow(results)) {
+  for(col in c('Set1', 'Set2', 'Set3', 'Set4', 'Set5')) {
+    if(nchar(results[[tolower(col)]][i]) == 2 & !is.na(results[[tolower(col)]][i])) {
+      results[[paste0('gamesWon', col)]][i] <- substr(results[[tolower(col)]][i], 1, 1)
+      results[[paste0('gamesLost', col)]][i] <- substr(results[[tolower(col)]][i], 2, 2)
+    } else if(!is.na(results[[tolower(col)]][i])) {
+      if(grepl('-', results[[tolower(col)]][i], fixed = TRUE)) {
+        results[[paste0('gamesWon', col)]][i] <- unlist(strsplit(results[[tolower(col)]][i], '-', fixed = TRUE))[1]
+        results[[paste0('gamesLost', col)]][i] <- unlist(strsplit(results[[tolower(col)]][i], '-', fixed = TRUE))[2]
+      } else {
+        stop('There is a result pattern that we did not account for.')
+      }
+    }
+  }
+}
+      
+for(col in c('Set1', 'Set2', 'Set3', 'Set4', 'Set5')) {
+  results[[paste0('gamesWon', col)]] <- as.numeric(results[[paste0('gamesWon', col)]])
+  results[[paste0('gamesLost', col)]] <- as.numeric(results[[paste0('gamesLost', col)]])
+}
+
+remove(col, i)      
+
+# score: Compute the number of sets
+results$numberOfSets <- rowSums(!is.na(results[, c('set1', 'set2', 'set3', 'set4', 'set5')]))
+
+results$numberOfSets[results$numberOfSets == 0] <- NA
+
+# eventPoints: Change the data type to numeric
+results$eventPoints <- as.numeric(results$eventPoints)
+
+# rank: Change the data type to numeric
+results$rank <- as.numeric(results$rank)
+
+# priceMoney: Separate the price money into the currency and value and change the data type to factor and numeric, respectively
+results$priceMoney <- gsub(',', '', results$priceMoney, fixed = TRUE)
+
+results$priceMoneyValue <- str_extract(results$priceMoney, '[:digit:]+')
+results$priceMoneyValue <- as.numeric(results$priceMoneyValue)
+
+results$priceMoneyCurrency <- str_extract(results$priceMoney, '.+?(?=[:digit:])')
+results$priceMoneyCurrency <- as.factor(results$priceMoneyCurrency)
+
+# priceMoney: Convert the price money into US dollar
+results$priceMoneyUSDollar <- NA
+
+for(i in 1:nrow(results)) {
+  currency <- results$priceMoneyCurrency[i]
+  value <- results$priceMoneyValue[i]
+  
+  if(currency == 'A$' & !is.na(currency)) {
+    results$priceMoneyUSDollar[i] <- value * australienDollarToUSDollar
+  } else if(currency == '€' & !is.na(currency)) {
+    results$priceMoneyUSDollar[i] <- value * euroToUSDollar
+  } else if(currency == '£' & !is.na(currency)) {
+    results$priceMoneyUSDollar[i] <- value * poundToUSDollar
+  } else if(currency == '$' & !is.na(currency)) {
+    results$priceMoneyUSDollar[i] <- value
+  } else {
+    if(!is.na(currency)) {
+      stop('There is a currency that we did not account for.')
+    }
+  }
+}
+
+remove(australienDollarToUSDollar, currency, euroToUSDollar, i, poundToUSDollar, value)
+
+# Add the general information on the opponents
+results <- left_join(results, players, by = c('opponentLink' = 'link'))
+
+# Harmonise the countries
+results$country.x <- countryname(results$country.x, 'ioc')
+
+results$country.x <- as.factor(results$country.x)
+
+# Rearrange the columns
+results <- results %>%
+  select(tournament, tournamentType, tournamentCity = city, tournamentCountry = country.x, tournamentStart,
+         tournamentEnd, outdoor, surface, priceMoneyTotalCurrency, priceMoneyTotalValue,
+         priceMoneyTotalUSDollar, rank, eventPoints, priceMoneyCurrency, priceMoneyValue,
+         priceMoneyUSDollar, round, opponent, opponentRank, opponentLink,
+         opponentCountry = country.y, opponentBirthday = birthday, opponentTurnedPro = turnedPro, opponentWeight = weight, opponentHeight = height,
+         opponentLefty = lefty, opponentOneHandedBackhand = oneHandedBackhand, win, walkover, retirement,
+         numberOfSets, starts_with('games'))
+
+# ANALYSE THE RESULTS OF THE PLAYER ---------------------------------------
+
+
